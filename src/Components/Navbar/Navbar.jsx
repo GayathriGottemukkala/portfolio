602a489d00c7ec80e492f0b3ff29ed14e7c858d8
pavankumar354 <cpavankumar354@gmail.com>
2024-10-29 16:35:19 +0530

    my changes

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useContext } from "react";
+import Toggle from "../Toggle/Toggle";
+import "./Navbar.css";
+import { Link } from "react-scroll";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { themeContext } from "../../Context";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`n-wrapper ${darkMode ? 'dark' : 'light'}`} id="Navbar">
+      <div className="n-left">
+        <div className="n-name">Gayathri</div>
+        <Toggle />
+      </div>
+      <div className="n-right">
+        <div
+          className="hamburger"
+          onClick={toggleMenu}
+          style={{ color: darkMode ? 'white' : 'black' }}
+        >
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size="2x" />
+        </div>
+        <div className={`n-list ${isOpen ? "open" : ""} ${darkMode ? 'dark' : 'light'}`}>
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <Link activeClass="active" to="Navbar" spy={true} smooth={true} onClick={toggleMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true} onClick={toggleMenu}>
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to="works" spy={true} smooth={true} onClick={toggleMenu}>
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link to="portfolio" spy={true} smooth={true} onClick={toggleMenu}>
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="testimonial" spy={true} smooth={true} onClick={toggleMenu}>
+                Testimonial
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="contact" spy={true} smooth={true}>
+          <button className="button n-button">Contact</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Portfolio/Portfolio.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Portfolio.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+
+import gamming from "../../img/gamming.png";
+import Weather from "../../img/weather.png";
+import Elearning from "../../img/elearning.png";
+import Bookstore from "../../img/bookstore.png";
+import Cowork from "../../img/cowork.png";
+import unstop from "../../img/blogs.png";
+
+const Portfolio = () => {
+    const projects = [
+        { src: unstop, link: 'https://unstop-blog-app.vercel.app/', text: 'Click to see my project' },
+        { src: gamming, link: 'https://gammingcart.vercel.app/', text: 'Click to see my project' },
+        { src: Weather, link: 'https://ajaymeru-weather-app.netlify.app/', text: 'Click to see my project' },
+        { src: Elearning, link: 'https://frabjous-bienenstitch-9c1be5.netlify.app/', text: 'Click to see my project' },
+        { src: Bookstore, link: 'https://boisterous-tulumba-bb607c.netlify.app/', text: 'Click to see my project' },
+        { src: Cowork, link: 'https://jay-co-working.netlify.app/', text: 'Click to see my project' },
+    ];
+
+    console.log("Projects:", projects);
+
+    return (
+        <div data-aos="fade-up">
+
+            <div className="portfolio" id="portfolio" >
+                <span>Recent Projects</span>
+                <span>Portfolio</span>
+            </div>
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                modules={[Navigation, Pagination, A11y, Autoplay]}
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                breakpoints={{
+                    640: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 40,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 50,
+                    },
+                }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {projects.map((project, index) => (
+                    <SwiperSlide key={index}>
+                        <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            <img src={project.src} alt={`Slide ${index + 1}`} />
+                            <div className="slide-text">{project.text}</div>
+                        </a>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>);
+};
+
+export default Portfolio;
